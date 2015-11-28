5b4f70b4059cf0772ba01a4e15a53c1aee51e453
Olivier Grisel <olivier.grisel@ensta.org>
2015-11-28 19:13:17 +0100

    WIP MemN2N that don't work

--- examples/babi_memnn.py
+++ examples/babi_memnn.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from keras.models import Sequential
 from keras.layers.embeddings import Embedding
-from keras.layers.core import Activation, Dense, Merge, Permute, Dropout
+from keras.layers.core import Activation, Dense, Merge, Permute, Dropout, TimeDistributedMerge
 from keras.layers.recurrent import LSTM
 from keras.datasets.data_utils import get_file
 from keras.preprocessing.sequence import pad_sequences
@@ -94,7 +94,7 @@ def vectorize_stories(data, word_idx, story_maxlen, query_maxlen):
         Xq.append(xq)
         Y.append(y)
     return (pad_sequences(X, maxlen=story_maxlen),
-            pad_sequences(Xq, maxlen=query_maxlen), np.array(Y))
+            pad_sequences(Xq, maxlen=query_maxlen), np.array(Y).astype(np.float32))
 
 
 path = get_file('babi-tasks-v1-2.tar.gz',
@@ -107,7 +107,7 @@ challenges = {
     # QA2 with 10,000 samples
     'two_supporting_facts_10k': 'tasks_1-20_v1-2/en-10k/qa2_two-supporting-facts_{}.txt',
 }
-challenge_type = 'two_supporting_facts_10k'
+challenge_type = 'single_supporting_fact_10k'
 challenge = challenges[challenge_type]
 
 print('Extracting stories for the challenge:', challenge_type)
@@ -139,69 +139,83 @@ inputs_test, queries_test, answers_test = vectorize_stories(test_stories, word_i
 print('-')
 print('inputs: integer tensor of shape (samples, max_length)')
 print('inputs_train shape:', inputs_train.shape)
+print('inputs_train dtype:', inputs_train.dtype)
 print('inputs_test shape:', inputs_test.shape)
+print('inputs_test dtype:', inputs_test.dtype)
 print('-')
 print('queries: integer tensor of shape (samples, max_length)')
 print('queries_train shape:', queries_train.shape)
+print('queries_train dtype:', queries_train.dtype)
 print('queries_test shape:', queries_test.shape)
+print('queries_test dtype:', queries_test.dtype)
 print('-')
 print('answers: binary (1 or 0) tensor of shape (samples, vocab_size)')
 print('answers_train shape:', answers_train.shape)
+print('answers_train dtype:', answers_train.dtype)
 print('answers_test shape:', answers_test.shape)
+print('answers_test dtype:', answers_test.dtype)
 print('-')
 t0 = time()
 print('Compiling...')
 
+EMBEDDING_SIZE = 64
+
 # embed the input sequence into a sequence of vectors
-input_encoder_m = Sequential()
-input_encoder_m.add(Embedding(input_dim=vocab_size,
-                              output_dim=64,
-                              input_length=story_maxlen))
+input_for_attention = Sequential()
+input_for_attention.add(Embedding(input_dim=vocab_size,
+                        output_dim=EMBEDDING_SIZE,
+                        input_length=story_maxlen))
 # output: (samples, story_maxlen, embedding_dim)
-# embed the question into a sequence of vectors
-question_encoder = Sequential()
-question_encoder.add(Embedding(input_dim=vocab_size,
-                               output_dim=64,
-                               input_length=query_maxlen))
+
+# embed the question into a vector using a simple sum
+# aggregate
+question = Sequential()
+question.add(Embedding(input_dim=vocab_size,
+                       output_dim=EMBEDDING_SIZE,
+                       input_length=query_maxlen))
 # output: (samples, query_maxlen, embedding_dim)
-# compute a 'match' between input sequence elements (which are vectors)
-# and the question vector sequence
-match = Sequential()
-match.add(Merge([input_encoder_m, question_encoder],
-                mode='dot',
-                dot_axes=[(2,), (2,)]))
-# output: (samples, story_maxlen, query_maxlen)
+question.add(TimeDistributedMerge(mode='sum'))
+
+print('question shape:', question.output_shape)
+# output: (samples, embedding_dim)
+
+# compute a 'attention' between input sequence elements (which are vectors)
+# and the question vector
+attention = Sequential()
+attention.add(Merge([input_for_attention, question],
+                    mode='dot', dot_axes=[(2,), (1,)]))
+attention.add(Activation('softmax'))
+print('attention shape:', attention.output_shape)
+# output: (samples, story_maxlen)
+
 # embed the input into a single vector with size = story_maxlen:
-input_encoder_c = Sequential()
-input_encoder_c.add(Embedding(input_dim=vocab_size,
-                              output_dim=query_maxlen,
-                              input_length=story_maxlen))
-# output: (samples, story_maxlen, query_maxlen)
-# sum the match vector with the input vector:
+input_for_response = Sequential()
+input_for_response.add(Embedding(input_dim=vocab_size,
+                                 output_dim=EMBEDDING_SIZE,
+                                 input_length=story_maxlen))
+# output: (samples, story_maxlen, embedding_dim)
+# sum the attention vectors with the input vector:
 response = Sequential()
-response.add(Merge([match, input_encoder_c], mode='sum'))
-# output: (samples, story_maxlen, query_maxlen)
-response.add(Permute((2, 1)))  # output: (samples, query_maxlen, story_maxlen)
+response.add(Merge([attention, input_for_response],
+                   mode='dot', dot_axes=[(1,), (1,)]))
 
-# concatenate the match vector with the question vector,
-# and do logistic regression on top
+print('response shape:', response.output_shape)
+# output: (samples, embedding_dim)
+
+# concatenate the attention vector with the question vector,
+# and do logistic or mlp regression on top
 answer = Sequential()
-answer.add(Merge([response, question_encoder], mode='concat', concat_axis=-1))
-# the original paper uses a matrix multiplication for this reduction step.
-# we choose to use a RNN instead.
-answer.add(LSTM(64))
-# one regularization layer -- more would probably be needed.
-answer.add(Dropout(0.25))
-answer.add(Dense(vocab_size))
-# we output a probability distribution over the vocabulary
-answer.add(Activation('softmax'))
+answer.add(Merge([response, question], mode='sum'))
 
-answer.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+# we output a probability distribution over the vocabulary
+answer.add(Dense(vocab_size, activation='softmax'))
+
+answer.compile(optimizer='adam', loss='categorical_crossentropy')
 # Note: you could use a Graph model to avoid repeat the input twice
 print('Model construction took %0.3fs' % (time() - t0))
 
 answer.fit([inputs_train, queries_train, inputs_train], answers_train,
            batch_size=32,
-           nb_epoch=70,
+           nb_epoch=500,
            show_accuracy=True,
            validation_data=([inputs_test, queries_test, inputs_test], answers_test))
